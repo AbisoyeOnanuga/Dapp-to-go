655b3a8a953f716b00c6f9f554f486b3fbc98535
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2023-09-17 18:06:30 -0400

    spheron site usage

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Require the dotenv package
+require("dotenv").config();
+
+// Import the spheron site sdk
+const { SpheronClient, ProtocolEnum } = require("@spheron/site");
+
+// Initialize the spheron site sdk with your token from the .env file
+const client = new SpheronClient({ token: process.env.SPHERON_TOKEN });
+
+// Initialize the spheron site sdk with your siteId and apiKey
+spheron.init(process.env.SITE_ID, process.env.SPHERON_TOKEN);
+
 'use strict';
 
 /* navbar toggle */
